711924904f70b1d83e1cd2d4ddcbe788c666d656
anderson03AyA <andersonsolarte03@gmail.com>
2023-04-06 17:21:45 -0500

    finalizado

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,48 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe, {
+  id: 19231868,
+  width: 640,
+});
+
+
+// working in the event on
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+//working in the event timeUpdate using throttle
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+
+// recargar la pagina desde el punto en el que estaba el video
+const currentTime = localStorage.getItem('videoplayer-current-time');
+function getCurrentTime(current) {
+  if (current === null || current === undefined) {
+    return 0;
+  }
+  return current;
+}
+player
+  .setCurrentTime(getCurrentTime(currentTime))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const input = document.querySelector('input');
+const textArea = document.querySelector('textarea');
+const boton = document.querySelector('button')
+
+// Obtener los datos del almacenamiento local y convertirlos en un objeto
+const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (formData) {
+  input.value = formData.email;
+  textArea.value = formData.message;
+}
+
+// evento input para actualizar el data storage
+input.addEventListener('input', throttle(handleFormInput, 500));
+textArea.addEventListener('input', throttle(handleFormInput, 500));
+
+function handleFormInput(event) {
+  const email = input.value;
+  const message = textArea.value;
+  const formData = {
+    email,
+    message,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+
+//enviar el formulario
+//1. borrar campos y data storage
+boton.addEventListener(
+  'click',
+  throttle(() => {
+    const email = '';
+    const message = '';
+    const formData = { email, message };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  }, 500)
+);
